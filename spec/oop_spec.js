80a8e561853e41e5a453de8dae94bdd4d2b0adb8
Yanga24 <60698180+Yanga24@users.noreply.github.com>
2020-03-10 13:00:21 +0200

    "first commit"

---
+++ spec/oop_spec.js
@@ -0,0 +1,30 @@
+let Person = require("../src/oop");
+
+let person = new Person(
+  "Ryan",
+  30,
+  "male",
+  "being a hardarse, agile, ssd hard drives"
+);
+
+describe("hello", function() {
+  person.sayHello();
+  it("should have name", function() {
+    expect(person.name).toBe("Ryan");
+  });
+
+  it("should have age", function() {
+    expect(person.age).toBe(30);
+  });
+
+  it("should have gender", function() {
+    expect(person.gender).toBe("male");
+  });
+  it("should have interest", function() {
+    expect(person.interest).toBe("being a hardarse, agile, ssd hard drives");
+  });
+
+  it("should return greeting", function() {
+    expect(person.sayHello).toBeDefined();
+  });
+});
